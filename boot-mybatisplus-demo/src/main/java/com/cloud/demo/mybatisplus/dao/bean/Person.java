faef39c7a76fca37e89ce205079a321d8fcbc673
jackZhou <zhouzhen01@gridsum.com>
2020-04-21 16:54:03 +0800

    校验类单独使用

--- boot-mybatisplus-demo/src/main/java/com/cloud/demo/mybatisplus/dao/bean/Person.java
+++ boot-mybatisplus-demo/src/main/java/com/cloud/demo/mybatisplus/dao/bean/Person.java
@@ -6,8 +6,15 @@ import com.baomidou.mybatisplus.annotation.TableField;
 import com.baomidou.mybatisplus.annotation.TableName;
 import lombok.Data;
 
+import javax.validation.ConstraintViolation;
+import javax.validation.Validation;
+import javax.validation.Validator;
+import javax.validation.ValidatorFactory;
 import javax.validation.constraints.Min;
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.groups.Default;
+import java.util.Set;
 
 /**
  * @ClassName Person
@@ -24,5 +31,18 @@ public class Person{
    @NotBlank
    private String name;
    @Min(16)
+   @NotNull
    private Integer age;
+
+   public static void main(String[] args) {
+      //单独使用 演示
+      Person person = new Person();
+      ValidatorFactory validatorFactory = Validation.buildDefaultValidatorFactory();
+      Validator validator = validatorFactory.getValidator();
+      Set<ConstraintViolation<Person>> validate = validator.validate(person, Default.class);
+      for(ConstraintViolation<Person> configConstraintViolation : validate){
+         System.out.println(configConstraintViolation.getPropertyPath() + configConstraintViolation.getMessage());
+      }
+
+   }
 }
