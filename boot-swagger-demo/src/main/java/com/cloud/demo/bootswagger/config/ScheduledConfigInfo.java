fe8daa8d32afc6be05ad6f58dda0a282d530dc34
zhouxiaohei <1012476676@qq.com>
2021-08-02 16:14:22 +0800

    Update and rename ScheduledConfigInfo to ScheduledConfigInfo.java

--- boot-swagger-demo/src/main/java/com/cloud/demo/bootswagger/config/ScheduledConfigInfo.java
+++ boot-swagger-demo/src/main/java/com/cloud/demo/bootswagger/config/ScheduledConfigInfo.java
@@ -1,3 +1,11 @@
+package com.cloud.demo.bootswagger.config;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+import java.util.concurrent.CopyOnWriteArrayList;
+
 @Component
 @ConfigurationProperties(prefix = "data.collect")
 public class ScheduledConfigInfo {
